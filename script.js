91d1ed66eaef4dd83b13f62e254b6a9fd44e7e5b
Glitch (plastic-lynx) <none>
2018-08-28 21:10:58 +0000

    🌒🐉 Checkpoint ./script.js:378534/1064 ./style.css:378534/31

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ let o = {
 }
 
 function point(pos, color) {
-  let c = 215 - Math.round((1 + Math.log(color) / Math.log(o.maxIterate)) * 215)
+  let c = 255 - Math.round((1 + Math.log(color) / Math.log(o.maxIterate)) * 255)
   c = c.toString(16)
 
   if (c.length === 1) {
-    o.canvas.fillStyle = '#00000' + c
+    o.canvas.fillStyle = '#AADFA' + c
   } else {
-    o.canvas.fillStyle = '#FFFF' + c
+    o.canvas.fillStyle = '#EEFF' + c
   }
 
   o.canvas.fillRect(pos[0], pos[1], 1, 1)
@@ -24,7 +24,7 @@ function point(pos, color) {
 function conversion(x, y, R) {
   const m = R / o.width
   const x1 = m * (2 * x - o.width)
-  const y2 = m * (o.width - 1.9 * y)
+  const y2 = m * (o.width - 2.9 * y)
   return [x1, y2]
 }
 
@@ -36,16 +36,20 @@ function abs(z) {
   return Math.sqrt(z[0] * z[0] + z[1] * z[1])
 }
 
-function init() {
-  const R = (1 + Math.sqrt(0.01 + .4 * abs(o.c))) / 1.2
-  let z, x, y, i
+const R = (1 + Math.sqrt(1 + 1.4 * abs(o.c))) / 2
+let z, x, y, i
 
+function init() {
   o.canvas = document.querySelector('canvas').getContext('2d')
-    
+}
+
+function render() {
+  init()
   for (x = 0; x < o.width; x++) {
     for (y = 0; y < o.length; y++) {
-      i = 0;
+      i = 0
       z = conversion(x, y, R)
+      
       while (i < o.maxIterate && abs(z) < R) {
         z = f(z, o.c)
         if (abs(z) > R) break
@@ -57,6 +61,7 @@ function init() {
       }
     }
   }
+  //requestAnimationFrame(render) 
 }
 
-init()
+render()
